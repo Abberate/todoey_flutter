d6704a5ca004427b448527cb30637704461b8f47
Abberate <abdoulaye222.ab@gmail.com>
2024-10-22 20:40:45 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/abibdigit/todoey_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.abibdigit.todoey_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:todoey_flutter/widgets/tasks_list.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<bool> etattache = [false, true, false, true, false];
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white,
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.lightBlueAccent,
+                      size: 30.0,
+                    )),
+                SizedBox(height: 10.0),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                      fontSize: 50.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '15  tâches',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(20.0),
+                      topLeft: Radius.circular(20.0))),
+              child: TasksList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
