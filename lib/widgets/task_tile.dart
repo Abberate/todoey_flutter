6d46c6efa3f2f23096a8a762cade474619801402
Abberate <abdoulaye222.ab@gmail.com>
2024-10-26 13:07:07 +0100

    commit-26-10-24

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isCheck = false;
+
+  void checkboxCallback(bool? checkboxState) {
+    setState(() {
+      isCheck = checkboxState ?? false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Faire du café'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
-        activeColor: Colors.lightBlueAccent,
-      ),
+      title: Text('Faire du café', style: TextStyle(decoration: isCheck ? TextDecoration.lineThrough : TextDecoration.none),),
+      trailing: TaskCheckbox(checkBoxState: isCheck, toggleCheckboxState: checkboxCallback),
+    );
+  }
+}
+
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkBoxState;
+  final ValueChanged<bool?>? toggleCheckboxState;
+  const TaskCheckbox({super.key,  required this.checkBoxState,required this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: toggleCheckboxState
     );
   }
 }
